e3ab64ee4d15d66c7037e28772af3e0df0c2bc56
victoai <victoai@naver.com>
2025-06-06 21:48:29 +0900

    start

---
+++ src/main/java/com/example/acorn_cicd1/AcornCicd1Application.java
@@ -0,0 +1,13 @@
+package com.example.acorn_cicd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcornCicd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcornCicd1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/acorn_cicd1/AcornCicd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acorn_cicd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcornCicd1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
