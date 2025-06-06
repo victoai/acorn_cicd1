7ad08498700d210db2e5993316ac1311690b646a
victoai <victoai@naver.com>
2025-06-06 21:49:28 +0900

    start

---
+++ src/main/java/com/example/acorn_cicd1/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.acorn_cicd1;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hi")
+    public String  hello(){
+        return  "hello";
+    }
+}
